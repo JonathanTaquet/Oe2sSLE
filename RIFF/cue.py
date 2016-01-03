b22e0bbd66c2ecac2b13d3bb83168a4d4289ee5e
Jonathan Taquet <jonathan.taquet@gmail.com>
2016-01-03 18:21:43 +0100

    start adding 'cue ' chunk

---
+++ RIFF/cue.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015 Jonathan Taquet
+
+This file is part of Oe2sSLE (Open e2sSample.all Library Editor).
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Oe2sSLE.  If not, see <http://www.gnu.org/licenses/>
+"""
+
+import struct
+import warnings
+
+import RIFF
+
+class RIFF_cue(RIFF.ChunkData):
+    _dataMinFmt = '<L'
+    _dataMinSize = struct.calcsize(_dataMinFmt)
+    
+    class CuePoint:
+        _dataFmt = '<2L4s3L'
+        _dataSize = struct.calcsize(_dataFmt)
+        
+        def __init__(self, cue_master, cue_point_num):
+            self.fields=dict()
+            self.cue = cue_master
+            offset=cue_master._dataMinSize+cue_point_num*self._dataSize
+            self.fields['identifier']=(offset, '<L')
+            offset+=struct.calcsize('L')
+            self.fields['position']=(offset, '<L')
+            offset+=struct.calcsize('L')
+            self.fields['fccChunk']=(offset, '<4s')
+            offset+=struct.calcsize('4s')
+            self.fields['chunkStart']=(offset, '<L')
+            offset+=struct.calcsize('L')
+            self.fields['blockStart']=(offset, '<L')
+            offset+=struct.calcsize('L')
+            self.fields['sampleOffset']=(offset, '<L')
+            offset+=struct.calcsize('L')
+            
+    
+        def __getattr__(self, name):
+            if name in self.fields:
+                loc, fmt = self.fields[name]
+                size = struct.calcsize(fmt)
+                unpacked = struct.unpack(fmt, self.cue.rawdata[loc:loc+size])
+                if len(unpacked) == 1:
+                    return unpacked[0]
+                else:
+                    return unpacked
+            else:
+                raise AttributeError
+    
+        def __setattr__(self, name, value):
+            if name != 'fields' and name in self.fields:
+                loc, fmt = self.fields[name]
+                size = struct.calcsize(fmt)
+                self.__dict__['cue'].rawdata[loc:loc+size] = struct.pack(fmt, value)
+            else:
+                self.__dict__[name] = value
+        
+    
+    def __init__(self, file=None, chunkHeader=None):
+        self.fields = dict()
+        self.rawdata = bytearray(RIFF_cue._dataMinSize)
+        offset = 0
+        self.fields['numCuePoints']=(offset, '<L')
+        offset += struct.calcsize('L')
+
+        self.cuePoints = []
+        
+        if file:
+            self.read(file,chunkHeader)
+        else:
+            self.reset()
+        
+#    def __len__(self):
+#        return len(self.rawdata)
+    
+    def read(self, file, chunkHeader):
+        if chunkHeader.id != b'cue ':
+            raise TypeError("'cue ' chunk expected")
+        self.rawdata[:] = file.read(chunkHeader.size)
+        if len(self.rawdata) != chunkHeader.size:
+            raise EOFError('Unexpected End Of File')
+        for cuePointNum in range(self.numCuePoints):
+            self.cuePoints.append(self.CuePoint(self,cuePointNum))
+        
+    def reset(self):
+        self.numCuePoints = 0
+        self.cuePoints = []
+
+#    def write(self, file):
+#        file.write(self.rawdata)
+
+    def __getattr__(self, name):
+        if name in self.fields:
+            loc, fmt = self.fields[name]
+            size = struct.calcsize(fmt)
+            unpacked = struct.unpack(fmt, self.rawdata[loc:loc+size])
+            if len(unpacked) == 1:
+                return unpacked[0]
+            else:
+                return unpacked
+        else:
+            raise AttributeError
+
+    def __setattr__(self, name, value):
+        if name != 'fields' and name in self.fields:
+            loc, fmt = self.fields[name]
+            size = struct.calcsize(fmt)
+            self.__dict__['rawdata'][loc:loc+size] = struct.pack(fmt, value)
+        else:
+            self.__dict__[name] = value
+
+
+    #TODO: easily add CuePoint(s)
