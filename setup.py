d6373c8547fa5ca834ce33cd2d1507ee95199ad0
Jonathan Taquet <jonathan.taquet@gmail.com>
2016-03-06 01:42:32 +0100

    update setup.py to bundle some files

--- setup.py
+++ setup.py
@@ -13,13 +13,25 @@ opts={
         #"includes": "mod1, mod2",
         "dist_dir": "Oe2sSLE-"+str(GUI.Oe2sSLE_VERSION[0])+"."+str(GUI.Oe2sSLE_VERSION[1])+"."+str(GUI.Oe2sSLE_VERSION[2])+"-win-x86",
         "excludes": ["pdb", "doctest", "distutils"],
-        "includes": ["tkinter", "pyglet.app", "pyglet.clock", "pyglet.resource"],
-        #"bundle_files": 2,
+        "includes": ["tkinter","imp"],
+        "bundle_files": 2,
         #"compressed": True,
         "optimize": 2
     }
 }
 
-setup(windows=['Oe2sSLE_GUI.py'],data_files=data,options=opts,
-	  #zipfile=None
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        self.version = str(GUI.Oe2sSLE_VERSION[0])+"."+str(GUI.Oe2sSLE_VERSION[1])+"."+str(GUI.Oe2sSLE_VERSION[2])
+        self.copyright = "Copyright (C) 2015-2016 Jonathan Taquet"
+        self.name = "Oe2sSLE"
+
+target = Target(
+    description = "Open e2sSample.all Library Editor for Electribe Sampler",
+    script = 'Oe2sSLE_GUI.py')
+
+setup(script_args=['py2exe'],
+      windows=[target],data_files=data,options=opts,
+	  zipfile=None
 	  )
