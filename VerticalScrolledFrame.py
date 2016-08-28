59ad77650b108eca1ebad8a4a693131c7f1cb6e1
Jonathan Taquet <jonathan.taquet@gmail.com>
2016-08-28 22:29:14 +0200

    fix: window refresh problem on Windows from e95a5f9
    This is a fix of previous fix e95a5f94aa7d27284ce00ee75cb067dd95e8fe7d.
    It had solved a refresh issue on Linux when the VerticalScrolledFrame
    interior frame size was changed. With the fix for Linux, it produces the same
    issue on Windows. This commit fix the issues.

--- VerticalScrolledFrame.py
+++ VerticalScrolledFrame.py
@@ -35,15 +35,12 @@ class VerticalScrolledFrame(tk.Frame):
             # update the scrollbars to match the size of the inner frame
             size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
             canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
-                canvas.itemconfigure(interior_id, width=interior.winfo_reqwidth())
+            # update the canvas's width to fit the inner frame
+            canvas.config(width=interior.winfo_reqwidth())
         interior.bind('<Configure>', _configure_interior)
 
         def _configure_canvas(event):
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+            # update the inner frame's width to fill the canvas
+            canvas.itemconfigure(interior_id, width=canvas.winfo_reqwidth)
         canvas.bind('<Configure>', _configure_canvas)
         self.canvas=canvas
