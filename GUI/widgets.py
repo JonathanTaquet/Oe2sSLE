82f06f0a9bafcabdb157abe76873501243aee367
Jonathan Taquet <jonathan.taquet@gmail.com>
2016-09-23 23:17:10 +0200

    add: GUI/widgets.py

---
+++ GUI/widgets.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015-2016 Jonathan Taquet
+
+This file is part of Oe2sSLE (Open e2sSample.all Library Editor).
+
+Oe2sSLE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Oe2sSLE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Oe2sSLE.  If not, see <http://www.gnu.org/licenses/>
+"""
+
