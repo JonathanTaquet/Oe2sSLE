5cb0ab668aa892aeb7942b673593e4d6428a7702
Jonathan Taquet <jonathan.taquet@gmail.com>
2016-10-16 22:04:55 +0200

    fix: copyright notices

--- e2s_sample_all.py
+++ e2s_sample_all.py
@@ -4,12 +4,12 @@ Copyright (C) 2015 Jonathan Taquet
 
 This file is part of Oe2sSLE (Open e2sSample.all Library Editor).
 
-Foobar is free software: you can redistribute it and/or modify
+Oe2sSLE is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+Oe2sSLE is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
