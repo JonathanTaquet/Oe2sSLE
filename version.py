e29d38870e8fe0d5ef6e8e39debbb010cdbc2322
Jonathan Taquet <jonathan.taquet@gmail.com>
2017-01-13 23:05:45 +0100

    version 0.0.13

--- version.py
+++ version.py
@@ -18,6 +18,6 @@ You should have received a copy of the GNU General Public License
 along with Oe2sSLE.  If not, see <http://www.gnu.org/licenses/>
 """
 
-Oe2sSLE_VERSION = (0,0,12)
+Oe2sSLE_VERSION = (0,0,13)
 
 debug = False
